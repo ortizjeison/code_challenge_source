1fe805d9866c09f411288b1ce1c6fbf62b21eb6b
ortizjeison-mint <jeison.ortiz@mint-data.co>
2025-03-06 14:02:14 -0500

    add docstrings

--- fetch_html.py
+++ fetch_html.py
@@ -5,9 +5,17 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 
 
-local = True
+local = False
 
 def fetch_html():
+    """
+    Fetches the HTML content of a specified website using a headless Chrome browser.
+    This function sets up Chrome options for headless browsing, initializes the WebDriver,
+    navigates to the specified URL, retrieves the page source (HTML), saves it to a file,
+    and then closes the WebDriver.
+    Returns:
+        str: The HTML content of the fetched website.
+    """
     # Set up Chrome options
     chrome_options = Options()
 
--- news_parser.py
+++ news_parser.py
@@ -5,7 +5,18 @@ import csv
 from entity_recognition import extract_entities
 
 
-def parse_news(html_content):
+def parse_news(html_content: str) -> list:
+    """
+    Parses HTML content to extract news articles.
+    Args:
+        html_content (str): The HTML content of the news page.
+    Returns:
+        list: A list of dictionaries, each containing the following keys:
+            - 'title' (str): The title of the article.
+            - 'kicker' (str): The kicker (headline) of the article.
+            - 'link' (str): The URL link to the full article.
+            - 'image' (str or None): The URL of the article's image, or None if no image is present.
+    """
 
     soup = BeautifulSoup(html_content, 'html.parser')
     parsed_articles = []
@@ -31,15 +42,30 @@ def parse_news(html_content):
 
     return parsed_articles
 
-def post_processing(parsed_articles: list):
+def post_processing(parsed_articles: list) -> pd.DataFrame:
+    """
+    Perform post-processing on a list of parsed articles and return a DataFrame.
+    This function takes a list of parsed articles and performs several post-processing steps:
+    - Converts the list of articles into a pandas DataFrame.
+    - Adds columns for word count, character count, and capitalized words in the article titles.
+    - Extracts named entities from the 'kicker' field and adds them as separate columns for persons, organizations, and locations.
+    - Saves the resulting DataFrame to a CSV file named 'articles.csv'.
+    Args:
+        parsed_articles (list): A list of dictionaries where each dictionary represents a parsed article.
+    Returns:
+        pandas.DataFrame: A DataFrame containing the post-processed article data.
+    """
+
     df = pd.DataFrame(parsed_articles)
 
     df['word_count_title'] = df['title'].apply(lambda x: len(x.split()))
     df['char_count_title'] = df['title'].apply(len)
     df['capital_words_title'] = df['title'].apply(lambda x: [word for word in x.split() if word.istitle()])
     
+    #Exctract the entities for each kicker and store them in a new column
     entities = df['kicker'].apply(lambda x: extract_entities(x))
     
+    #Add the entities to the dataframe on separated  columns
     df['persons'] = entities.apply(lambda x: ', '.join(list(set(x.get("PERSON", [])))))
     df['organizations'] = entities.apply(lambda x: ', '.join(list(set(x.get("ORG", [])))))
     df['locations'] = entities.apply(lambda x: ', '.join(list(set(x.get("GPE", [])))))
