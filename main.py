f7c814cbc355ca886bb1ad05baf3fa683d85ef29
ortizjeison-mint <jeison.ortiz@mint-data.co>
2025-03-06 21:52:07 -0500

    Update logic: always fetch html

--- main.py
+++ main.py
@@ -3,16 +3,10 @@ from fetch_html import fetch_html
 from news_parser import parse_news, post_processing
 import os
 from gcp_utils import upload_dataframe_to_bigquery
-from env_manager import local
 
 def run_web_scraping():
 
-    if local():
-        with open('tmp/website.html', 'r') as file:
-            html_content = file.read()
-    else:
-        #Fetch html content with selenium
-        html_content = fetch_html()
+    html_content = fetch_html()
 
     articles = parse_news(html_content)
 
